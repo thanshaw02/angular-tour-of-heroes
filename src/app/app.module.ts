163c8b6dbc7c1351b13ed8543857d3a041196fac
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-03-09 19:16:31 -0500

    add in memory web api stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services';
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import {
@@ -16,12 +19,20 @@ import {
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,7 +1,9 @@
 import HeroService from "./heroService/hero.service";
+import InMemoryDataService from "./inMemoryService/in-memory-data.service";
 import MessageService from "./messageService/message.service";
 
 export {
   HeroService,
+  InMemoryDataService,
   MessageService
 };
